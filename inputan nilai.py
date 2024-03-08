8250fdcba9c4e6da2ac7d09000730c31272f70da
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2024-03-08 10:43:15 +0700

    Add files via upload

---
+++ inputan nilai.py
@@ -0,0 +1,8 @@
+nilai = int (input("masukan nilai anda: "))
+
+if nilai >75:
+    hasil ="anda dinyatakan lulus"
+else:
+    hasil ="sayang sekali anda tidak lulus"
+    
+print(hasil)
---
+++ tahun lahir.py
@@ -0,0 +1,7 @@
+tahun_lahir = int (input("masukan tahun lahir anda:"))
+if tahun_lahir >= 2000:
+    hasil = "anda merupakan genZ"
+else:
+    hasil = "wah ternyata anda bukan genZ"
+
+print(hasil)
